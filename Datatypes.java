59e4d5846afdc0de71722bf6a94bbb3838fa3388
abhinay <63472859+abhinay-developer@users.noreply.github.com>
2021-05-30 09:45:26 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,28 @@
+/*
+*
+*Java Program DataTypes 
+*/
+class  Datatypes{
+
+public static void main(String args[]){
+
+byte b=0;
+short s=0;
+int i=0;
+long l=0;
+float f=0.0F;
+double d=0.0;
+String str=null;
+
+System.out.println("Byte Value::"+b);
+System.out.println("ShortValue::"+s);
+System.out.println("Integer Value::"+i);
+
+System.out.println("LongValue::"+l);
+System.out.println("Float::"+f);
+System.out.println("Double Value::"+d);
+System.out.println("String Value ::"+str);
+
+}
+
+}
---
+++ Datatypes2.java
@@ -0,0 +1,28 @@
+/*
+*
+*Java Program DataTypes 
+*/
+class  Datatypes2{
+
+public static void main(String args[]){
+
+byte  day=7;
+short  days=365;
+int age=25;
+long mobileNumber=9955665533L;
+float price=700.0F;
+double amount=100000.2525;
+String name="Raja";
+
+System.out.println("Day Value::"+day);
+System.out.println("Days Value::"+days);
+System.out.println("Age Value::"+age);
+
+System.out.println("MobileNumber::"+mobileNumber);
+System.out.println("Price ::"+price);
+System.out.println("Amount::"+amount);
+System.out.println("Name ::"+name);
+
+}
+
+}
